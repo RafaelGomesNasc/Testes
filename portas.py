3b1c840279fa21cc891570bc728323cc36011659
Rafael Gomes do Nascimento <161648281+RafaelGomesNasc@users.noreply.github.com>
2025-02-13 08:34:01 -0300

    Update portas.py

--- portas.py
+++ portas.py
@@ -9,7 +9,6 @@ class PORT(ABC):
     def eval(self):
         pass
 
-
 class NOT(PORT):
     def __init__(self, entrada = False):
         # Verificando se a entrada é um valor booleano ou inteiro
@@ -44,7 +43,7 @@ class AND(PORT):
         self.args = args
 
     def eval(self, *args):
-
+        # Se for passado um valor para args, ele atualiza o valor de self.args
         if len(args) >= 2:
             for a in args:
                 if not isinstance(a, (bool, int)):
@@ -52,32 +51,121 @@ class AND(PORT):
                 if isinstance(a, int) and a not in (0, 1):
                     raise ValueError("Entrada deve ser 0 ou 1")
             self.args = args
-
+        # Se não for passado um valor para args, ele pega o valor de self.args
         else:
             args = self.args
-
-        res = True
-        for a in args:
-            res = res and a
-        return res
+        # Retorna o valor lógico da operação AND de todas as entradas
+        return all(args)
 
 class OR(PORT):
-    pass
+    def __init__(self, *args):
+        for a in args:
+            if not isinstance(a, (bool, int)):
+                raise TypeError("Entrada deve ser um valor booleano ou inteiro")
+            if isinstance(a, int) and a not in (0, 1):
+                raise ValueError("Entrada deve ser 0 ou 1")
+        self.args = args
+
+    def eval(self, *args):
+        # Se for passado um valor para args, ele atualiza o valor de self.args
+        if len(args) >= 2:
+            for a in args:
+                if not isinstance(a, (bool, int)):
+                    raise TypeError("Entrada deve ser um valor booleano ou inteiro")
+                if isinstance(a, int) and a not in (0, 1):
+                    raise ValueError("Entrada deve ser 0 ou 1")
+            self.args = args
+        # Se não for passado um valor para args, ele pega o valor de self.args
+        else:
+            args = self.args
+        # Retorna o valor lógico da operação OR de todas as entradas
+        return any(args)
 
 
 class XOR(PORT):
-    pass
+    def __init__(self, *args):
+        for a in args:
+            if not isinstance(a, (bool, int)):
+                raise TypeError("Entrada deve ser um valor booleano ou inteiro")
+            if isinstance(a, int) and a not in (0, 1):
+                raise ValueError("Entrada deve ser 0 ou 1")
+        self.args = args
+
+    def eval(self, *args):
+        # Se for passado um valor para args, ele atualiza o valor de self.args
+        if len(args) >= 2:
+            for a in args:
+                if not isinstance(a, (bool, int)):
+                    raise TypeError("Entrada deve ser um valor booleano ou inteiro")
+                if isinstance(a, int) and a not in (0, 1):
+                    raise ValueError("Entrada deve ser 0 ou 1")
+            self.args = args
+        # Se não for passado um valor para args, ele pega o valor de self.args
+        else:
+            args = self.args
+        # Retorna o valor lógico da operação XOR de todas as entradas
+        return sum(args) == 1
 
 
 class NAND(PORT):
-    pass
+    def __init__(self, *args):
+        for a in args:
+            if not isinstance(a, (bool, int)):
+                raise TypeError("Entrada deve ser um valor booleano ou inteiro")
+            if isinstance(a, int) and a not in (0, 1):
+                raise ValueError("Entrada deve ser 0 ou 1")
+        self.args = args
 
+    def eval(self, *args):
+        # Se for passado um valor para args, ele atualiza o valor de self.args
+        if len(args) >= 2:
+            for a in args:
+                if not isinstance(a, (bool, int)):
+                    raise TypeError("Entrada deve ser um valor booleano ou inteiro")
+                if isinstance(a, int) and a not in (0, 1):
+                    raise ValueError("Entrada deve ser 0 ou 1")
+            self.args = args
+        # Se não for passado um valor para args, ele pega o valor de self.args
+        else:
+            args = self.args
+        # Retorna o valor lógico da operação NAND de todas as entradas
+        return not all(args)
+    
 
 class NOR(PORT):
-    pass
+    def __init__(self, *args):
+        for a in args:
+            if not isinstance(a, (bool, int)):
+                raise TypeError("Entrada deve ser um valor booleano ou inteiro")
+            if isinstance(a, int) and a not in (0, 1):
+                raise ValueError("Entrada deve ser 0 ou 1")
+        self.args = args
+
+    def eval(self, *args):
+        # Se for passado um valor para args, ele atualiza o valor de self.args
+        if len(args) >= 2:
+            for a in args:
+                if not isinstance(a, (bool, int)):
+                    raise TypeError("Entrada deve ser um valor booleano ou inteiro")
+                if isinstance(a, int) and a not in (0, 1):
+                    raise ValueError("Entrada deve ser 0 ou 1")
+            self.args = args
+        # Se não for passado um valor para args, ele pega o valor de self.args
+        else:
+            args = self.args
+        # Retorna o valor lógico da operação NOR de todas as entradas
+        return not any(args)
 
 
 if __name__ == "__main__":
+    #-------------------------------------#
+    # PORT
+    #-------------------------------------#
+    try:
+        port = PORT()
+        print(port.eval())
+    except Exception as e:
+        print(e)
     #-------------------------------------#
     # NOT
     #-------------------------------------#
@@ -107,3 +195,52 @@ if __name__ == "__main__":
     #-------------------------------------#
     # OR
     #-------------------------------------#
+    try:
+        or1 = OR(0, 0)
+        print(or1.eval())
+    except Exception as e:
+        print(e)
+    try:
+        or2 = OR(0, 1, 0)
+        print(or2.eval())
+    except Exception as e:
+        print(e)
+    #-------------------------------------#
+    # XOR
+    #-------------------------------------#
+    try:
+        xor1 = XOR(1, 1)
+        print(xor1.eval())
+    except Exception as e:
+        print(e)
+    try:
+        xor2 = XOR(1, 0, 1)
+        print(xor2.eval())
+    except Exception as e:
+        print(e)
+    #-------------------------------------#
+    # NAND
+    #-------------------------------------#
+    try:
+        nand1 = NAND(1, 1)
+        print(nand1.eval())
+    except Exception as e:
+        print(e)
+    try:
+        nand2 = NAND(1, 0, 1)
+        print(nand2.eval())
+    except Exception as e:
+        print(e)
+    #-------------------------------------#
+    # NOR
+    #-------------------------------------#
+    try:
+        nor1 = NOR(0, 0)
+        print(nor1.eval())
+    except Exception as e:
+        print(e)
+    try:
+        nor2 = NOR(0, 1, 0)
+        print(nor2.eval())
+    except Exception as e:
+        print(e)
